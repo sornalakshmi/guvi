0e8cc3c0d7e90a36fff0191b5eb4e99b46f01bad
sornalakshmi <abiramithiagu20@gmail.com>
2016-11-10 14:05:45 +0530

    Update IndexMatchNumber.java

--- IndexMatchNumber.java
+++ IndexMatchNumber.java
@@ -7,12 +7,17 @@ class IndexMatchNumber
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-	      int[] arr={-3,-1,1,3,5};
-              for(int i=0;i<arr.length;i++){
-                if(arr[i]>i)
-                  break;
-                else if(arr[i]==i)
-                  System.out.println(arr[i]);
-              }
+		int flag=0;
+	      	int[] arr={-3,-1,1,3,5};
+              	for(int i=0;i<arr.length;i++){
+                	if(arr[i]>i)
+                  		break;
+                	else if(arr[i]==i){
+                  		System.out.println(arr[i]);
+		  		flag=1;
+			}
+		}
+		if(flag==0)
+			System.out.println("No Match Found");
           }
 }
